27e4e76212d2021a838ed93aed5a13522b52968b
Andrea Cingolani <cingolaniandrea99@gmail.com>
2026-06-24 21:08:11 +0200

    Refined the structure. Rendered the diary more flexible.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
+/*
+    Copyright (C) 2026  Andrea Cingolani
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+use std::collections::{HashMap, HashSet};
 use std::fs::{File, OpenOptions, copy, create_dir_all, read_to_string};
 use std::io::{Write, stdin, stdout};
 
-use chrono::{DateTime, Local, MappedLocalTime, NaiveDateTime, TimeZone, Utc};
+use chrono::{Local, MappedLocalTime, NaiveDateTime, TimeZone};
 use clap::{Parser, Subcommand};
 use color_eyre::eyre::{Report, Result};
 use directories::ProjectDirs;
@@ -10,8 +28,23 @@ use serde_json;
 
 const TIME_FMT: &str = "%d/%m/%y %H:%M:%S";
 
+#[derive(Debug, Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
 #[derive(Debug, Subcommand)]
 enum Commands {
+    /// Manage and display active fields
+    ActiveFields {
+        /// Add some active fields (comma-separated, eg. --add "field1,...")
+        #[arg(short, long, value_delimiter = ',')]
+        add: Option<Vec<String>>,
+        /// Remove some active fields (comma-separated, eg. --remove "field1,...")
+        #[arg(short, long, value_delimiter = ',')]
+        remove: Option<Vec<String>>,
+    },
     /// Add an entry to the diary
     AddEntry {
         /// Manually set a different timestamp than the current. Time of the entry must be in "DD/MM/YY HH:MM:SS" format
@@ -21,38 +54,66 @@ enum Commands {
     /// Modify an entry in the diary
     ChangeEntry {
         /// Time of the entry in format "DD/MM/YY HH:MM:SS"
+        #[arg(short, long)]
         timestamp: String,
     },
     /// Remove an entry from the diary
     RemoveEntry {
         /// Time of the entry in format "DD/MM/YY HH:MM:SS"
+        #[arg(short, long)]
         timestamp: String,
     },
     /// Print the diary on the screen
     PrintDiary,
 }
 
-#[derive(Debug, Parser)]
-struct Cli {
-    #[command(subcommand)]
-    command: Commands,
+#[derive(Debug, Deserialize, Serialize)]
+struct Diary {
+    active_fields: HashSet<String>,
+    diary_entries: Vec<DiaryEntry>,
 }
 
 #[derive(Debug, Default, Deserialize, Serialize)]
 struct DiaryEntry {
     timestamp: i64,
-    mood: u8,
-    anxiety: u8,
-    sadness: u8,
-    anger: u8,
-    tiredness: u8,
-    restlessness: u8,
+    date: String,
+    fields: HashMap<String, i8>,
     note: Option<String>,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-struct Diary {
-    diary_entries: Vec<DiaryEntry>,
+impl DiaryEntry {
+    // Construct a new DiaryEntry asking the user
+    fn from_user_input(
+        timestamp: i64,
+        date: String,
+        active_fields: &HashSet<String>,
+    ) -> Result<Self> {
+        let mut entry = DiaryEntry::default();
+
+        entry.timestamp = timestamp;
+        entry.date = date;
+
+        for field in active_fields {
+            let mut user_input = String::new();
+            print!("{}: ", field);
+            stdout().flush()?;
+            stdin().read_line(&mut user_input)?;
+            entry
+                .fields
+                .insert(field.clone(), user_input.trim().parse()?);
+        }
+
+        let mut user_input = String::new();
+        print!("Note: ");
+        stdout().flush()?;
+        stdin().read_line(&mut user_input)?;
+        let user_input = user_input.trim().to_string();
+        if !user_input.is_empty() {
+            entry.note = Some(user_input)
+        }
+
+        Ok(entry)
+    }
 }
 
 fn main() -> Result<()> {
@@ -77,18 +138,11 @@ fn main() -> Result<()> {
         diary_path
     };
 
-    // Check if directory already exists or if it path is something else
-    if !data_dir_path.exists() {
-        create_dir_all(&data_dir_path)?;
-    } else if !data_dir_path.is_dir() {
-        let name = data_dir_path.to_string_lossy();
-        return Err(Report::msg(format!(
-            "Object with name '{}' already exists.",
-            name
-        )));
-    }
+    // Create directory if doesn't exist already
+    create_dir_all(&data_dir_path)?;
 
-    // Define the file handle opening or creating it
+    // Define the file handle opening or creating the default
+    let mut diary_isnt_new = true;
     let mut diary = {
         if !diary_path.exists() {
             File::create_new(&diary_path)?;
@@ -97,7 +151,33 @@ fn main() -> Result<()> {
         let diary_contents = read_to_string(&diary_path)?;
 
         if diary_contents.trim().is_empty() {
+            diary_isnt_new = false;
+            let mut active_fields: HashSet<String>;
+            println!("Your diary seems empty. Let's start by adding some active fields.");
+            loop {
+                println!("Write a string of values (comma-separated): ");
+                let mut user_input = String::new();
+                stdin().read_line(&mut user_input)?;
+                active_fields = user_input
+                    .trim()
+                    .split(',')
+                    .filter_map(|field| {
+                        let field = field.trim();
+                        if field.is_empty() {
+                            None
+                        } else {
+                            Some(field.to_string())
+                        }
+                    })
+                    .collect();
+                if active_fields.is_empty() {
+                    continue;
+                } else {
+                    break;
+                }
+            }
             Diary {
+                active_fields,
                 diary_entries: Vec::new(),
             }
         } else {
@@ -106,14 +186,54 @@ fn main() -> Result<()> {
     };
 
     match cli.command {
+        // Manage the active fields
+        Commands::ActiveFields { add, remove } => {
+            let mut display_fields = true;
+
+            if let Some(add) = add {
+                display_fields = false;
+                let add: Vec<_> = add
+                    .into_iter()
+                    .map(|field| field.trim().to_string())
+                    .collect();
+                diary.active_fields.extend(add);
+            }
+
+            if let Some(remove) = remove {
+                display_fields = false;
+                let remove: Vec<_> = remove
+                    .into_iter()
+                    .map(|field| field.trim().to_string())
+                    .collect();
+                remove.iter().for_each(|field| {
+                    diary.active_fields.remove(field);
+                });
+            }
+
+            if display_fields {
+                let active_fields_str = diary
+                    .active_fields
+                    .iter()
+                    .cloned()
+                    .collect::<Vec<_>>()
+                    .join(", ");
+                println!("{active_fields_str}");
+
+                // The diary has not been modified so don't bother updating the file, unless is new
+                if diary_isnt_new {
+                    return Ok(());
+                }
+            }
+        }
         // Handle adding a new entry to the diary
         Commands::AddEntry {
             timestamp: timestring,
         } => {
-            let timestamp = if let Some(timestring) = timestring {
-                timestring_to_timestamp(&timestring)?
+            let (timestamp, date) = if let Some(timestring) = timestring {
+                (timestring_to_timestamp(&timestring)?, timestring)
             } else {
-                Utc::now().timestamp()
+                let now = Local::now();
+                (now.timestamp(), now.format(TIME_FMT).to_string())
             };
 
             if let Some(old_entry) = diary
@@ -128,14 +248,14 @@ fn main() -> Result<()> {
                 return Err(report);
             }
 
-            let new_entry = DiaryEntry::from_user_input(timestamp)?;
+            let new_entry = DiaryEntry::from_user_input(timestamp, date, &diary.active_fields)?;
             diary.diary_entries.push(new_entry);
         }
         // Handle changing an entry in the diary
         Commands::ChangeEntry {
             timestamp: timestring,
         } => {
-            let timestamp = timestring_to_timestamp(&timestring)?;
+            let (timestamp, date) = (timestring_to_timestamp(&timestring)?, timestring);
 
             if let Some(old_entry) = diary
                 .diary_entries
@@ -143,8 +263,8 @@ fn main() -> Result<()> {
                 .find(|old_diary_entry| old_diary_entry.timestamp == timestamp)
             {
                 println!("Old diary entry:\n{:#?}\nNew entry:\n", old_entry);
-
-                *old_entry = DiaryEntry::from_user_input(timestamp)?;
+                let old_entry_fields: HashSet<_> = old_entry.fields.keys().cloned().collect();
+                *old_entry = DiaryEntry::from_user_input(timestamp, date, &old_entry_fields)?;
             } else {
                 let report = Report::msg("No entry with the given timestamp found.");
                 return Err(report);
@@ -186,43 +306,18 @@ fn main() -> Result<()> {
                 return Err(report);
             }
         }
-        // Print all the entries in a CSV format
+        // Print all the entries
         Commands::PrintDiary => {
             for entry in &diary.diary_entries {
-                let DiaryEntry {
-                    timestamp,
-                    mood,
-                    anxiety,
-                    sadness,
-                    anger,
-                    tiredness,
-                    restlessness,
-                    note,
-                } = entry;
-
-                let note = if let Some(entry_note) = note {
-                    entry_note.as_str()
-                } else {
-                    ""
-                };
-
-                let utc_datetime = DateTime::from_timestamp_secs(*timestamp);
-                let local_datetime = if let Some(datetime) = utc_datetime {
-                    let naive_datetime = datetime.naive_utc();
-                    Local
-                        .from_utc_datetime(&naive_datetime)
-                        .format(TIME_FMT)
-                        .to_string()
-                } else {
-                    String::from("out-of-range number of seconds")
-                };
-
-                println!(
-                    "{},{},{},{},{},{},{},{}",
-                    local_datetime, mood, anxiety, sadness, anger, tiredness, restlessness, note
-                );
+                println!("===== {} =====", entry.date);
+                for (field, val) in &entry.fields {
+                    println!("{field}: {val}");
+                }
+                if let Some(note) = &entry.note {
+                    println!("{note}");
+                }
+                println!();
             }
-
             // The diary has not been modified so don't bother updating the file
             return Ok(());
         }
@@ -232,7 +327,7 @@ fn main() -> Result<()> {
     diary.diary_entries.sort_by_key(|entry| entry.timestamp);
 
     // Serialize the new contents in the diary
-    let mut new_diary_content = serde_json::to_vec(&diary)?;
+    let mut new_diary_content = serde_json::to_vec_pretty(&diary)?;
     new_diary_content.push(b'\n');
 
     // Define the path to the backup copy of the diary e copy the current diary
@@ -273,62 +368,5 @@ caused by for example missing time zone data files, an error in an OS API, or ov
 
             Err(report)
         }
-    }
-}
-
-impl DiaryEntry {
-    // Construct a new DiaryEntry asking the user
-    fn from_user_input(timestamp: i64) -> Result<Self> {
-        let mut input = String::new();
-        let mut entry = DiaryEntry::default();
-
-        entry.timestamp = timestamp;
-
-        print!("Mood: ");
-        stdout().flush()?;
-        stdin().read_line(&mut input)?;
-        entry.mood = input.trim().parse()?;
-        input.clear();
-
-        print!("Anxiety: ");
-        stdout().flush()?;
-        stdin().read_line(&mut input)?;
-        entry.anxiety = input.trim().parse()?;
-        input.clear();
-
-        print!("Sadness: ");
-        stdout().flush()?;
-        stdin().read_line(&mut input)?;
-        entry.sadness = input.trim().parse()?;
-        input.clear();
-
-        print!("Anger: ");
-        stdout().flush()?;
-        stdin().read_line(&mut input)?;
-        entry.anger = input.trim().parse()?;
-        input.clear();
-
-        print!("Tiredness: ");
-        stdout().flush()?;
-        stdin().read_line(&mut input)?;
-        entry.tiredness = input.trim().parse()?;
-        input.clear();
-
-        print!("Restlessness: ");
-        stdout().flush()?;
-        stdin().read_line(&mut input)?;
-        entry.restlessness = input.trim().parse()?;
-        input.clear();
-
-        print!("Note: ");
-        stdout().flush()?;
-        stdin().read_line(&mut input)?;
-        entry.note = if input.trim().is_empty() {
-            None
-        } else {
-            Some(input.trim().to_string())
-        };
-
-        Ok(entry)
     }
 }
